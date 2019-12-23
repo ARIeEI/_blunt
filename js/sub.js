46501c7874ddba9ac9ecc603c1d51befdad113c7
ARIeEI <charmant1001@gmail.com>
2019-12-23 23:44:55 +0900

    refactor_pallax 함수화.
    index.js에 있는 scrollAnimation함수로 인해, 불필요해진 기존 코드 삭제

--- js/sub.js
+++ js/sub.js
@@ -1,40 +1,4 @@
-
 //VIVUS
 new Vivus('textBoxStroke1',{duration:60},function(){$('.textBoxStroke').attr('style','fill:#fff');});
 new Vivus('textBoxStroke2',{duration:60},function(){$('.textBoxStroke').attr('style','fill:#fff');});
 new Vivus('textBoxStroke3',{duration:60},function(){$('.textBoxStroke').attr('style','fill:#fff');});
-
-//모바일 분기
-// function parallax (){
-//         $(window).scroll(function(){
-//                 if($(window).width() >= 768){
-//                         $(window).scroll(function(){                    
-//                         var th = $(this);	
-//                         sAniM2(th,aniu,'AniU');	 
-//                         sAniM2(th,anid,'AniD');
-//                         });
-//                 }
-//                 var th = $(this);
-//                 sAniM2(th,aniu,'AniU');	 
-//                 sAniM2(th,anid,'AniD');
-//         });
-// }
-
-//페러럭스 함수화.            
-//mobile
-// function starAniM2(th, vv, mm) {
-//         if (th.scrollTop() > vv.offset().top - 780) {
-//             vv.addClass(mm);
-//         } else {
-//             vv.removeClass(mm);
-//         }
-//     }
-    
-//     //pc
-//     function starAni2(th, vv, mm) {
-//         if (th.scrollTop() > vv.offset().top - 1290) {
-//             vv.addClass(mm);
-//         } else {
-//             vv.removeClass(mm);
-//         }
-//     }
